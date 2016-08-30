e72ef3c10c5322e42000449e29046e2849435af5
luperca <hotolete@gmail.com>
2016-08-30 22:21:18 +0200

    Creo clase App y llamo a la clase Ventana que es un JFrame.

---
+++ Hilos/src/consola/App.java
@@ -0,0 +1,10 @@
+package consola;
+
+import visual.Ventana;
+
+public class App {
+
+    public static void main(String[] args) {
+        new Ventana().setVisible(true);
+    }
+}//fin class consola.App
